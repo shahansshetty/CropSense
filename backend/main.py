1e8f81e90bbf1be19d6f3496a295a08debf30f68
Shahan S Shetty <shahansshetty@gmail.com>
2025-11-18 22:46:05 +0530

    First commit

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import pickle
+from markupsafe import escape
+
+app=Flask(__name__)
+
+with open("crop_rec_model.pkl","rb") as f:
+    model=pickle.load(f)
+
+
+
+@app.route('/predict')
+def index():
+
+    N=request.args.get("N")
+    P=request.args.get("P")
+    K=request.args.get("K")
+    temp=request.args.get("T")
+    Humidity=request.args.get("H")
+    pH=request.args.get("pH")
+    RainFall=request.args.get("R")
+
+    data=[[N,P,K,temp,Humidity,pH,RainFall]]
+    prediction=model.predict(data)
+    return {"crop":prediction[0]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
