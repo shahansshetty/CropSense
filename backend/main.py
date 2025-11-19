656c5985c52af2c7a19a937d261a81b49efb7403
Shahan S Shetty <shahansshetty@gmail.com>
2025-11-19 19:45:40 +0530

    added requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import pickle
-from markupsafe import escape
+# from markupsafe import escape
 
 app=Flask(__name__)
 
@@ -9,20 +9,22 @@ with open("crop_rec_model.pkl","rb") as f:
 
 
 
-@app.route('/predict')
+@app.route('/predict',methods=['GET'])
 def index():
+    if request.method  =='GET':
+        N=request.args.get("N")
+        P=request.args.get("P")
+        K=request.args.get("K")
+        temp=request.args.get("T")
+        Humidity=request.args.get("H")
+        pH=request.args.get("pH")
+        RainFall=request.args.get("R")
 
-    N=request.args.get("N")
-    P=request.args.get("P")
-    K=request.args.get("K")
-    temp=request.args.get("T")
-    Humidity=request.args.get("H")
-    pH=request.args.get("pH")
-    RainFall=request.args.get("R")
-
-    data=[[N,P,K,temp,Humidity,pH,RainFall]]
-    prediction=model.predict(data)
-    return {"crop":prediction[0]}
+        data=[[N,P,K,temp,Humidity,pH,RainFall]]
+        prediction=model.predict(data)
+        return {"crop":prediction[0]}
+    else:
+        return "Improper http request"
 
 if __name__ == "__main__":
     app.run(debug=True)
